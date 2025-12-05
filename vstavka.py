5a0f63a656d840850e5dd0048fcb67a4ab7aff09
olgashulga-lev <rubyshulga@gmail.com>
2025-12-05 15:50:32 +0300

    Сортировка вставками

---
+++ vstavka.py
@@ -0,0 +1,10 @@
+n = int(input())
+a = list(map(int, input().split()))
+for i in range(1, n):
+    x = a[i]
+    j = i - 1
+    while j >= 0 and a[j] > x:
+        a[j + 1] = a[j]
+        j -= 1
+    a[j + 1] = x
+print(*a)
